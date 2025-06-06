7c393e3f5acd18baac8dbcdcde9164e61faa7237
adisak <67319010092@swdtcmail.com>
2025-06-06 14:31:55 +0700

    67319010092

---
+++ popuo.js
@@ -0,0 +1,19 @@
+window.onload = () => {
+  const popup = document.getElementById('popup');
+  const closeBtn = document.getElementById('closePopupBtn');
+
+  // แสดง popup ทันทีเมื่อโหลดหน้าเว็บ
+  popup.style.display = 'block';
+
+  // ปิด popup
+  closeBtn.addEventListener('click', () => {
+    popup.style.display = 'none';
+  });
+
+  // ปิด popup เมื่อคลิกพื้นหลัง
+  window.addEventListener('click', (event) => {
+    if (event.target === popup) {
+      popup.style.display = 'none';
+    }
+  });
+};
